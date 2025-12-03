bc33457905fdbb88f0fee950d639e3882ac3af2d
Soumya Purani <66162598+SoumyaPurani@users.noreply.github.com>
2025-12-03 19:23:06 -0400

    adding categories

---
+++ src/main/java/com/ecommerce/sb_ecomm/model/Category.java
@@ -0,0 +1,27 @@
+package com.ecommerce.sb_ecomm.model;
+
+public class Category {
+    private Long category_id;
+    private String category_name;
+
+    public Category(Long category_id, String category_name) {
+        this.category_id = category_id;
+        this.category_name = category_name;
+    }
+
+    public String getCategory_name() {
+        return category_name;
+    }
+
+    public void setCategory_name(String category_name) {
+        this.category_name = category_name;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
+}
