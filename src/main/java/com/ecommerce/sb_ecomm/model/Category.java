fd6bd1ced8f34b2de453c1e0d90ec84ba687b3a3
Soumya Purani <66162598+SoumyaPurani@users.noreply.github.com>
2025-12-06 23:24:12 -0400

    Implementing Lombok to reduce boilerplate code

--- src/main/java/com/ecommerce/sb_ecomm/model/Category.java
+++ src/main/java/com/ecommerce/sb_ecomm/model/Category.java
@@ -4,36 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long category_id;
     private String category_name;
-
-    public Category(Long category_id, String category_name) {
-        this.category_id = category_id;
-        this.category_name = category_name;
-    }
-
-    public Category() {
-
-    }
-
-    public String getCategory_name() {
-        return category_name;
-    }
-
-    public void setCategory_name(String category_name) {
-        this.category_name = category_name;
-    }
-
-    public Long getCategory_id() {
-        return category_id;
-    }
-
-    public void setCategory_id(Long category_id) {
-        this.category_id = category_id;
-    }
 }
