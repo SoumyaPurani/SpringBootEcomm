dd62ee55ad717e8e16c256a99323036344fff6f6
Soumya Purani <66162598+SoumyaPurani@users.noreply.github.com>
2025-12-08 16:03:59 -0400

    Adding Exception Handler

---
+++ src/main/java/com/ecommerce/sb_ecomm/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.ecommerce.sb_ecomm.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> response = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(err -> {
+            String FieldName = ((FieldError)err).getField();
+            String ErrorMessage = err.getDefaultMessage();
+            response.put(FieldName, ErrorMessage);
+        });
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.BAD_REQUEST);
+    }
+}
