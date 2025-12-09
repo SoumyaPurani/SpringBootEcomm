068358ee16cd1aaf99557d27141fd2f4dcf20614
Soumya Purani <66162598+SoumyaPurani@users.noreply.github.com>
2025-12-09 19:02:06 -0400

    Implemented Resource not found Exception

--- src/main/java/com/ecommerce/sb_ecomm/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecomm/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.sb_ecomm.service;
 
+import com.ecommerce.sb_ecomm.exceptions.ResourceNotFoundException;
 import com.ecommerce.sb_ecomm.model.Category;
 import com.ecommerce.sb_ecomm.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,14 +28,14 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String deleteCategory(Long category_id) {
-        Category category = categoryRepository.findById(category_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        Category category = categoryRepository.findById(category_id).orElseThrow(() -> new ResourceNotFoundException("Category", "Category id", category_id));
         categoryRepository.delete(category);
         return category.getCategory_id() + " has been deleted";
     }
 
     @Override
     public Category updateCategory(Category category, Long category_id) {
-        Category savedCategory = categoryRepository.findById(category_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        Category savedCategory = categoryRepository.findById(category_id).orElseThrow(() -> new ResourceNotFoundException("Category", "Category id", category_id));
         category.setCategory_id(category_id);
         savedCategory = categoryRepository.save(category);
         return savedCategory;
